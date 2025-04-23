a56352e954af9806c4e947482b6569fead25498a
Kenza-sh <kenza@goodman-ai.com>
2025-04-23 16:45:55 +0200

    Update __init__.py

--- get_adresse_postale/__init__.py
+++ get_adresse_postale/__init__.py
@@ -1,7 +1,5 @@
 import azure.functions as func
 import logging
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 import re
 import json
 import urllib.request
@@ -11,38 +9,6 @@ from typing import List, Dict
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-'''def load_ner_model():
-    logger.info("Chargement du modèle NER...")
-    tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    return pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
-
-nlp = load_ner_model()
-
-class InformationExtractor:
-    def __init__(self , nlp_pipeline):
-        self.nlp=nlp_pipeline
-        logger.info("Modèle NER initialisé avec succès.")      
-    def extraire_adresse(self, texte):
-        logger.info(f"Extraction de l'adresse à partir du texte : {texte}")
-        # Extraction du numéro de rue
-        numero_rue = re.search(r'\b\d+\b', texte)
-        adr = f"{numero_rue.group()} " if numero_rue else ""
-        adr=''
-        # Extraction des entités pertinentes
-        entities = self.nlp(texte)
-        for ent in entities:
-            if ent['entity_group'] in {"LOC", "PER"}:
-                adr += ent['word'] + ' '
-        adr = adr.strip()
-        if adr:
-            logger.info(f"Adresse extraite : {adr}")
-        else:
-            logger.warning("Aucune adresse n'a été extraite.")
-        return adr
-
-extractor = InformationExtractor(nlp)
-'''
 class InformationExtractor:
     def __init__(self):
         logger.info("Modèle NER initialisé avec succès.")    
