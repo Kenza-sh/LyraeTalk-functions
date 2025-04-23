36f62974e2f3a95d7552b9e6b66e3d93443d2b05
Kenza-sh <kenza@goodman-ai.com>
2025-04-23 17:15:47 +0200

    Update __init__.py

--- get_nom_famille/__init__.py
+++ get_nom_famille/__init__.py
@@ -1,25 +1,87 @@
 import azure.functions as func
 import logging
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 import re
 import json
+import urllib.request
+import os
+from typing import List, Dict
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def load_ner_model():
-    logger.info("Chargement du modèle NER...")
-    tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
-    return pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
-
-nlp = load_ner_model()
 
 class InformationExtractor:
-    def __init__(self , nlp_pipeline):
-        self.nlp=nlp_pipeline
+    def __init__(self ):
         logger.info("Modèle NER initialisé avec succès.")
+        
+    def get_entities(self , texte):
+        data = {"inputs": texte}
+        body = str.encode(json.dumps(data))
+        url = os.environ["HG_MODEL_ENDPOINT"]
+        api_key = os.environ["HG_MODEL_ENDPOINT_KEY"]
+        if not api_key:
+            raise Exception("A key should be provided to invoke the endpoint")
+        headers = {'Content-Type':'application/json', 'Accept': 'application/json', 'Authorization':('Bearer '+ api_key)}
+        req = urllib.request.Request(url, body, headers)
+        try:
+            response = urllib.request.urlopen(req)
+            result = response.read()
+            decoded_str = result.decode('utf-8')
+            ner_list = json.loads(decoded_str)
+            logger.info(result)
+            logger.info(ner_list)
+            return ner_list
+        except urllib.error.HTTPError as error:
+            logger.info("The request failed with status code: " + str(error.code))
+            logger.info(error.info())
+            logger.info(error.read().decode("utf8", 'ignore'))
+            return []
+            
+    def reconstruct_entities(self, ner_output):
+        entities = []
+        current_entity = {
+            "entity": None,
+            "score": [],
+            "start": None,
+            "end": None,
+            "word": ""
+        }
+    
+        for token in ner_output:
+            if current_entity["entity"] != token["entity"]:
+                if current_entity["entity"]:
+                    entities.append({
+                        "entity": current_entity["entity"],
+                        "score": sum(current_entity["score"]) / len(current_entity["score"]),
+                        "word": current_entity["word"].replace("▁", " ").strip(),
+                        "start": current_entity["start"],
+                        "end": current_entity["end"]
+                    })
+                # Start new entity
+                current_entity = {
+                    "entity": token["entity"],
+                    "score": [token["score"]],
+                    "start": token["start"],
+                    "end": token["end"],
+                    "word": token["word"]
+                }
+            else:
+                current_entity["score"].append(token["score"])
+                current_entity["end"] = token["end"]
+                current_entity["word"] += token["word"]
+    
+        # Add last entity
+        if current_entity["entity"]:
+            entities.append({
+                "entity": current_entity["entity"],
+                "score": sum(current_entity["score"]) / len(current_entity["score"]),
+                "word": current_entity["word"].replace("▁", " ").strip(),
+                "start": current_entity["start"],
+                "end": current_entity["end"]
+            })
+    
+        return entities
+        
     def check_noun(self, msg_2_check):
         logger.debug(f"Vérification du nom : {msg_2_check}")
         def check_str(msg_2_check: str) -> bool:
@@ -33,9 +95,11 @@ class InformationExtractor:
         return True
     def extraire_nom(self, texte):
         logger.info(f"Extraction du nom à partir du texte : {texte}")
-        entities = self.nlp(texte)
+        entities = self.get_entities(texte)
+        logger.info(entities)
+        entities= self.reconstruct_entities(entities)
         for ent in entities:
-            if ent['entity_group'] == "PER":
+            if ent['entity'] == "I-PER":
                 if self.check_noun(ent['word'].lower()):
                     logger.info(f"Nom extrait : {ent['word'].upper()}")
                     return ent['word'].upper()
@@ -43,7 +107,7 @@ class InformationExtractor:
         return None
       
     
-extractor = InformationExtractor(nlp)
+extractor = InformationExtractor()
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
